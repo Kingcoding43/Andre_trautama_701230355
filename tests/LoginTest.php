fef2503b5dd7e11f950ed59655ede8ca182bfcee
Andre Trautama <andretaurapratama@gmail.com>
2025-11-21 02:48:08 +0700

    Update LoginTest.php

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -3,9 +3,14 @@ use PHPUnit\Framework\TestCase;
 
 class LoginTest extends TestCase
 {
-    public function testDummy()
+    public function test_login_file_exists()
     {
-        $this->assertTrue(true);
+        $this->assertFileExists('login.php');
+    }
+
+    public function test_login_page_loads()
+    {
+        $content = file_get_contents('login.php');
+        $this->assertIsString($content);
     }
 }
-
